0c9f102ab55d84aa9dea5ffca32ce2a5f1bbeaf3
cwen0708 <cwen0708@gmail.com>
2016-10-25 23:38:36 +0800

    Remove unused permission settings

--- __init__.py
+++ __init__.py
@@ -10,17 +10,6 @@ plugins_helper = {
     "title": u"線上編輯原始碼",
     "desc": u"可以以動態的方式進行程式碼編輯",
     "controllers": {
-        "code": {
-            "group": u"原始碼",
-            "actions": [
-                {"action": "list", "name": u"輪撥圖管理"},
-                {"action": "add", "name": u"新增輪撥圖"},
-                {"action": "edit", "name": u"編輯輪撥圖"},
-                {"action": "view", "name": u"檢視輪撥圖"},
-                {"action": "delete", "name": u"刪除輪撥圖"},
-                {"action": "plugins_check", "name": u"啟用停用模組"},
-            ]
-        },
         "code_target": {
             "group": u"原始碼",
             "actions": [
